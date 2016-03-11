be86643b6457117774c47dd47ada0f47b8a6500a
Terrance J Peters <terrancejpeters@gmail.com>
2016-03-10 23:51:12 -0500

    Non-recursive divide state

--- src/sorting/MergeSorter.java
+++ src/sorting/MergeSorter.java
@@ -10,123 +10,133 @@ import structures.Queue;
  * 
  * e1 is less than or equal to e2 if and only if (e1.compareTo(e2) <= 0)
  *
- * You may not use loops (for, while, do, etc.) in this class. You must
- * instead use recursion.
+ * You may not use loops (for, while, do, etc.) in this class. You must instead
+ * use recursion.
  */
 public class MergeSorter<T extends Comparable<T>> {
 	/**
-	 * Returns a new queue containing the elements from the input queue
-	 * in sorted order.
+	 * Returns a new queue containing the elements from the input queue in
+	 * sorted order.
 	 * 
 	 * Do not modify the input queue! Work on a copy of the input.
 	 * 
 	 * Implement this method recursively:
 	 * 
-	 *   In the base case, return the sorted queue.
+	 * In the base case, return the sorted queue.
 	 *
-	 *   Otherwise:
+	 * Otherwise:
 	 * 
-	 *     First, divide the input queue into two smaller output queues.
+	 * First, divide the input queue into two smaller output queues.
 	 * 
-	 *     Then, recursively mergeSort each of these smaller queues. 
+	 * Then, recursively mergeSort each of these smaller queues.
 	 * 
-	 *     Finally, return the result of merging these two queues.
+	 * Finally, return the result of merging these two queues.
 	 * 
-	 * @param queue an input queue
+	 * @param queue
+	 *            an input queue
 	 * @return a sorted copy of the input queue
 	 */
 	public Queue<T> mergeSort(Queue<T> queue) {
 		// TODO 1
-		return null;
+		Queue<T> split1 = new Queue<T>();
+		Queue<T> split2 = new Queue<T>();
+		divide(queue, split1, split2);
+		return merge(split1, split2);
+
 	}
 
 	/**
-	 * Places elements from the input queue into the output queues, roughly
-	 * half and half.
+	 * Places elements from the input queue into the output queues, roughly half
+	 * and half.
 	 * 
 	 * Implement this method recursively:
 	 * 
-	 *   In the base case, there's nothing left to do.
-	 *   
-	 *   Otherwise:
-	 *   
-	 *     Make progress on moving elements from the input to the output.
-	 *     
-	 *     Then make a recursive call to divide.
-	 *   
-	 * @param input a queue
-	 * @param output1 a queue into which about half of the elements in input should go
-	 * @param output2 a queue into which the other half of the elements in input should go
+	 * In the base case, there's nothing left to do.
+	 * 
+	 * Otherwise:
+	 * 
+	 * Make progress on moving elements from the input to the output.
+	 * 
+	 * Then make a recursive call to divide.
+	 * 
+	 * @param input
+	 *            a queue
+	 * @param output1
+	 *            a queue into which about half of the elements in input should
+	 *            go
+	 * @param output2
+	 *            a queue into which the other half of the elements in input
+	 *            should go
 	 */
 	void divide(Queue<T> input, Queue<T> output1, Queue<T> output2) {
 		int tempSize = input.size();
-		for (int i = 0; i < tempSize/2; i++){
+		for (int i = 0; i < tempSize / 2; i++) {
 			output1.enqueue(input.dequeue());
 		}
 
-		while (!input.isEmpty()){
-			output2.enqueue(input.dequeue());	
+		while (!input.isEmpty()) {
+			output2.enqueue(input.dequeue());
 		}
-		
+
 	}
-	
+
 	/**
-	 * Merges sorted input queues into an output queue in sorted order,
-	 * and returns that queue. 
+	 * Merges sorted input queues into an output queue in sorted order, and
+	 * returns that queue.
 	 * 
 	 * Use mergeHelper to accomplish this goal.
-	 *  
-	 * @param input1 a sorted queue
-	 * @param input2 a sorted queue
+	 * 
+	 * @param input1
+	 *            a sorted queue
+	 * @param input2
+	 *            a sorted queue
 	 * @return a sorted queue consisting of all elements from input1 and input2
 	 */
 	Queue<T> merge(Queue<T> input1, Queue<T> input2) {
 		Queue<T> output = new Queue<T>();
-		
-		return null;
-		}
-	
+		mergeHelper(input1, input2, output);
+		return output;
+	}
+
 	/**
 	 * Merges the sorted input queues into the output queue in sorted order.
 	 * 
 	 * Implement this method recursively:
 	 * 
-	 *   In the base case, there's nothing left to do.
-	 *   
-	 *   Otherwise:
-	 *   
-	 *     Make progress on moving elements from an input to the output.
-	 *     
-	 *     Then make a recursive call to mergeHelper.
-	 *     
-	 * @param input1 a sorted queue
-	 * @param input2 a sorted queue
-	 * @param output a sorted queue containing the accumulated progress so far
+	 * In the base case, there's nothing left to do.
+	 * 
+	 * Otherwise:
+	 * 
+	 * Make progress on moving elements from an input to the output.
+	 * 
+	 * Then make a recursive call to mergeHelper.
+	 * 
+	 * @param input1
+	 *            a sorted queue
+	 * @param input2
+	 *            a sorted queue
+	 * @param output
+	 *            a sorted queue containing the accumulated progress so far
 	 */
 	void mergeHelper(Queue<T> input1, Queue<T> input2, Queue<T> output) {
-		if (input1.isEmpty() && input2.isEmpty()){
-			
-		}
-		else{
-			if (input1.isEmpty()){
+		if (input1.isEmpty() && input2.isEmpty()) {
+		} else {
+			if (input1.isEmpty()) {
 				output.enqueue(input2.dequeue());
-			}
-			else if (input2.isEmpty()){
+			} else if (input2.isEmpty()) {
 				output.enqueue(input1.dequeue());
-			}
-			else{
+			} else {
 				T elem1 = input1.dequeue();
 				T elem2 = input2.dequeue();
-				if (elem1.compareTo(elem2) >= 0){
+				if (elem1.compareTo(elem2) >= 0) {
 					output.enqueue(elem1);
 					output.enqueue(elem2);
-				}
-				else{
+				} else {
 					output.enqueue(elem2);
 					output.enqueue(elem1);
 				}
 			}
-			mergeHelper(input1,input2,output);
+			mergeHelper(input1, input2, output);
 		}
 	}
 }
